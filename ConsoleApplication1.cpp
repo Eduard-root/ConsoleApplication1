0e9478cf841e07aad532ea6edee7c44150f3d5c1
Gleb Lukin <gleblukin144@gmail.com>
2025-06-16 14:54:48 +0300

    Размер

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 int main()
 {
+    setlocale(0, "");
     std::cout << "Hello World!\n";
     cout << "Privet vsem\n";
 
     string ptr;
     ptr = "privet";
     cout << ptr << endl;
-    cout << ptr.empty() << endl;
+    cout << "Размер " << ptr.size() << endl;
 }
